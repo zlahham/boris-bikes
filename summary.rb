811965e80a34a0eca1b02957f775d4c48bdd36a7
Fadi-Hakim <Fadihakim@msn.com>
2015-07-21 17:46:43 +0100

    Added summary

---
+++ summary.rb
@@ -0,0 +1,11 @@
+#1 Create an object on irb
+#2 Take note of the error
+#3 make a spec file to test for the object
+#4 define the Class
+#5 we check for the same error, then we run rspec to find that its fixed
+#6 in irb we call a method on the object, which would fail
+#7 alter the spec file for the object
+#8 Add the method to the class
+#9 test rspec
+#10 error should be fixxed
+#code,test,fix repeat 
