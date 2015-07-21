801e4eb88cf4df7ff822a7941c0fd4a82a04f5a4
zlahham <zlahham@gmail.com>
2015-07-21 17:56:16 +0100

    Completed Challenge 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+require_relative 'bike'
+
 class DockingStation
   def release_bike
+    Bike.new # => Added this line to create a Bike class when we call
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+
   it 'docking station releases bike' do
     expect(subject).to respond_to :release_bike
   end
+
+  it 'gets a bike that is working' do
+    instance = Bike.new
+    expect(instance).to be_working
+  end
+
 end
