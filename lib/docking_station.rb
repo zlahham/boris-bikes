b3616e8436204dbcc807907ab96ce6c3b0a9667c
Sivan Patel <sivan.patel@googlemail.com>
2015-07-22 11:52:42 +0100

    Post Challenge 1 pt 1

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,11 @@ require_relative 'bike'
 
 class DockingStation
   def release_bike
-    Bike.new # => Added this line to create a Bike class when we call
+    fail 'No bikes available' # => Added this line to create a Bike class when we call
   end
+
+  def dock bike
+
+  end
+
 end
